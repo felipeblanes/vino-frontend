38aee3479fed0fe820b6e4ae005e3c4e89018d55
Felipe Blanes <felipe.blanes@hotmail.com>
2020-12-19 11:50:10 -0500

    Verificacao de preenchimento de campos de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,24 +9,38 @@ import { ArmazenamentoService } from '../armazenamento.service';
 })
 export class LoginComponent implements OnInit {
   usuario: any;
+  mensagemErro = false;
+  mensagem = "";
 
   constructor(private armazenamentoService: ArmazenamentoService, private router: Router) { 
     this.usuario = armazenamentoService.usuario;
   }
 
   ngOnInit(): void {
+    this.usuario.autenticacao = false
   }
 
   fazerLogin(inputNome: HTMLInputElement, inputSenha: HTMLInputElement) {
-    this.usuario.email = inputNome.value;
+    this.usuario.nome = inputNome.value;
     this.usuario.senha = inputSenha.value;
-
-    console.log(this.usuario.email + " " + this.usuario.senha);  
-    this.router.navigate(['/video-streaming']);
 
     this.armazenamentoService.verificarSenha()
     console.log(this.usuario.autenticacao);
+    console.log(this.usuario.nome);
 
+    if(this.usuario.nome == ''){
+      this.mensagemErro = true;
+      this.mensagem = 'Por favor, insira o seu nome completo.';
+    } else if(this.usuario.senha == ''){
+      this.mensagemErro = true;
+      this.mensagem = 'Por favor, insira a senha do evento.';
+    } else if(this.usuario.autenticacao == true){
+      this.router.navigate(['/video-streaming']);
+    } else{
+      this.mensagemErro = true;
+      this.mensagem = 'Senha do evento incorreta. Por favor, verifique com o organizador do evento.';
+    }
+    
   }
 
 }
